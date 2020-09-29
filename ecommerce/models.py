50464805fa04731db6a2014dc9b2982ffa6807db
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-09-29 00:14:46 +0000

    made a good url

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,33 +1,40 @@
 from django.db import models
-from django.template.defaultfilters import  slugify
 from django.shortcuts import reverse
+from django.template.defaultfilters import slugify
+
+from hashids import Hashids
+
+
 # Create your models here.
+hashids = Hashids()
+
 class Base_Model(models.Model):
     date_add = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
-        abstract =True
+        abstract = True
+
 
 class Category(Base_Model):
-    name = models.CharField(max_length=200,unique=True)
+    name = models.CharField(max_length=200, unique=True)
 
     def get_absolute_url(self):
         return reverse("category_page")
-    
 
     def __str__(self):
         return self.name
-    
+
     class Mata:
-        verbose_name_plural ='categories'
+        verbose_name_plural = 'categories'
+
 
 class Sub_Category(Base_Model):
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='sub_categories')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='sub_categories')
     name = models.CharField(max_length=200)
 
-     # create a new slug
+    # create a new slug
     # def save(self, *args, **kwargs):
     #     if not self.slug:
     #         self.slug = slugify(self.name)
@@ -35,30 +42,40 @@ class Sub_Category(Base_Model):
 
     def __str__(self):
         return f'{self.category} : {self.name}'
-    
+
     class Meta:
         verbose_name = 'Sub Category'
         verbose_name_plural = 'Sub Categories'
 
-class Product(Base_Model):
-    category = models.ForeignKey(Sub_Category,on_delete=models.CASCADE,related_name='products')
-    name=models.CharField(max_length=100)
-    image = models.FileField(upload_to='images/%Y/%m/%d/')
-    price=models.DecimalField(decimal_places=2,max_digits=20)
-    description = models.TextField(blank=True, null=True)
-    is_available= models.BooleanField(default=True)
-    slug = models.SlugField(unique=True,editable=False,null=True,blank=True)
 
+class Product(Base_Model):
+    category = models.ForeignKey(
+        Sub_Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    image = models.FileField(upload_to='images/%Y/%m/%d/')
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    description = models.TextField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, editable=False, null=True, blank=True)
 
     # create a new slug
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        return super().save(*args, **kwargs)
 
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #     return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse("product-detail", kwargs={"slug": self.slug})
-    
+
     def __str__(self):
         return f'{self.name}: {self.category}'
+
+
+    def save(self, *args, **kwargs):
+        id_ = Product.objects.values_list('pk', flat=True).count()
+        id_ = str(hashids.encode(id_))
+        slug = slugify(self.name)
+        self.slug =  slug+'-'+id_
+        return super().save(*args, **kwargs)
+
